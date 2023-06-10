0c25ef5f2da6dcb5225ff6a293bf751ceb12b9c2
Christopher Mullins <pantherakrugeri@gmail.com>
2023-06-10 06:51:32 -0500

    short inline event with arrow function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 function App () {
     return (
         <div>
-            <button onClick={() => {
-                console.log('btn was clicked!');
-            }}>Add Animal</button>
+            <button onClick={() => console.log('btn was clicked!')}>Add Animal</button>
         </div>
     )
 }
