3857e25ceaceffa97111945160d970e10abaf4e4
Christopher Mullins <pantherakrugeri@gmail.com>
2023-06-10 06:43:51 -0500

    inline event function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 function App () {
-    const handleClick = () => {
-        console.log('btn was clicked!');
-    }
-
     return (
         <div>
-            <button onClick={handleClick}>Add Animal</button>
+            <button onClick={function() {
+                console.log(console.log('btn was clicked!')
+            }}>Add Animal</button>
         </div>
     )
 }
