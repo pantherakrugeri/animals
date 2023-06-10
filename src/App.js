2bc247105bc695d9e6640df084ff6cc94f136f4a
Christopher Mullins <pantherakrugeri@gmail.com>
2023-06-10 06:47:53 -0500

    inline arrow event variation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App () {
     return (
         <div>
-            <button onClick={function() {
-                console.log(console.log('btn was clicked!')
+            <button onClick={() => {
+                console.log('btn was clicked!');
             }}>Add Animal</button>
         </div>
     )
