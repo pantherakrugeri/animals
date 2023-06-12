03c5a28e73ea4215c1488e0f99e6da8b71b79121
Christopher Mullins <pantherakrugeri@gmail.com>
2023-06-12 09:37:12 -0500

    broke out handleclick

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+
 function App () {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    }
+
     return (
         <div>
-            <button onClick={() => console.log('btn was clicked!')}>Add Animal</button>
+            <button onClick={handleClick}>Add Animal</button>
+            <div>Animals to show: {count}</div>
         </div>
     )
 }
